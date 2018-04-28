2385c9afc9d3a56257b47815952cd4509ec1387e
Melissa Jacobini <melissa.jacobini@gmail.com>
2018-04-28 11:31:30 -0500

    events add to-do

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,9 +5,9 @@ class ToDo extends Component{
     render(){
         return(
             <li>
-            <input type="checkbox" checked={ this.props.isCompleted } />
-            <span>{ this.props.description }</span>
-          </li>
+          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+         <span>{this.props.description}</span>
+       </li>
         );
     }
 }
